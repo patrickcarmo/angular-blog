0de00a051ee66a1611ae804f9b505f860a970e33
Patrick Carmo <patrick.carmo@wolterskluwer.com>
2021-03-06 14:06:09 +0000

    added pipe to sort an array by property

---
+++ src/app/pipes/sort-property.pipe.ts
@@ -0,0 +1,31 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sortByProperty',
+})
+export class SortByProperty implements PipeTransform {
+  transform(
+    value: any[] | null,
+    property: string,
+    sort: 'ASC' | 'DESC'
+  ): any[] | null {
+    return !!value
+      ? value.sort((a, b) => {
+          if (sort === 'ASC') {
+            if (a[property] < b[property]) {
+              return -1;
+            } else if (a[property] > b[property]) {
+              return 1;
+            }
+          } else {
+            if (a[property] > b[property]) {
+              return -1;
+            } else if (a[property] < b[property]) {
+              return 1;
+            }
+          }
+          return 0;
+        })
+      : value;
+  }
+}
