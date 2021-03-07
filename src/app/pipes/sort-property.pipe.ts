505211716d78d0fcdaaef3c90eb5649121a97cf4
Patrick Carmo <patrick.carmo@wolterskluwer.com>
2021-03-07 02:40:00 +0000

    improved pipe name

--- src/app/pipes/sort-property.pipe.ts
+++ src/app/pipes/sort-property.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'sortByProperty',
 })
-export class SortByProperty implements PipeTransform {
+export class SortByPropertyPipe implements PipeTransform {
   transform(
     value: any[] | null,
     property: string,
