010384aee80b2cf67bb8416cfc0f57f08b50cdfd
Patrick Carmo <patrick.carmo@wolterskluwer.com>
2021-03-07 02:43:07 +0000

    added component comment list

---
+++ src/app/comment/comment-list/comment-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Comment } from 'src/app/models/comments.model';
+
+@Component({
+  selector: 'app-comment-list',
+  templateUrl: './comment-list.component.html',
+  styleUrls: ['./comment-list.component.scss'],
+})
+export class CommentListComponent implements OnInit {
+  @Input() comments: Comment[] = [];
+
+  @Output() commented = new EventEmitter<number>();
+
+  showButtonReply = false;
+
+  currentComment: Comment | null = null;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggleComment(close: boolean): void {
+    console.log('aqui');
+    this.showButtonReply = close;
+  }
+}
