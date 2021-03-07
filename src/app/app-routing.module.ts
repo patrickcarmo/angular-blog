bba070e9eee9c413f4d9b1ec2cf9fcd6b848d8ce
Patrick Carmo <patrick.carmo@wolterskluwer.com>
2021-03-07 02:52:04 +0000

    added the route to BlogPostDetailComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogOverviewComponent } from './blog/blog-overview/blog-overview.component';
+import { BlogPostDetailComponent } from './blog/blog-post-detail/blog-post-detail.component';
 
 const routes: Routes = [
   {
@@ -8,6 +9,10 @@ const routes: Routes = [
     component: BlogOverviewComponent,
   },
   { path: '', redirectTo: 'blog', pathMatch: 'full' },
+  {
+    path: 'blog/:id',
+    component: BlogPostDetailComponent,
+  },
 ];
 
 @NgModule({
