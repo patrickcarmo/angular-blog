4f8891f64428d1cac2cd28ae20a343b240b01716
Patrick Carmo <patrick.carmo@wolterskluwer.com>
2021-03-07 15:26:19 +0000

    removed the unnecessary file and the import

--- src/app/published-by/published-by.component.ts
+++ src/app/published-by/published-by.component.ts
@@ -3,7 +3,6 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-published-by',
   templateUrl: './published-by.component.html',
-  styleUrls: ['./published-by.component.scss'],
 })
 export class PublishedByComponent implements OnInit {
   @Input() author = '';
